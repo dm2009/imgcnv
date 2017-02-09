ea016feeef6cd4d1568fc9e4d1735a01b0b4d36c
Dmitry_Slepchenkov <Dmitry_Slepchenkov@epam.com>
2017-02-09 14:58:12 +0300

    fix try catch error

--- src/main/java/org/imgcnv/service/DownloadServiceImpl.java
+++ src/main/java/org/imgcnv/service/DownloadServiceImpl.java
@@ -2,6 +2,7 @@ package org.imgcnv.service;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -11,7 +12,6 @@ import org.imgcnv.exception.PersistentException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-//import org.apache.commons.validator.UrlValidator;
 import org.apache.commons.validator.routines.UrlValidator;
 
 public class DownloadServiceImpl implements DownloadService {
@@ -24,18 +24,23 @@ public class DownloadServiceImpl implements DownloadService {
     public long download(String url, Path destination) {
         long result = -1;
         UrlValidator urlValidator = new UrlValidator();
-
+        URL uri = null;
         if (urlValidator.isValid(url)) {
             try {
-                URL uri = new URL(url);
-                try (InputStream in = uri.openStream()) {
-                    Files.copy(in, destination, StandardCopyOption.REPLACE_EXISTING);
-                }
+                uri = new URL(url);
+            } catch (MalformedURLException e) {
+                result = -1;
+                throw new PersistentException(e);
+            }
+        }
+        
+        if (uri!=null) {
+            try (InputStream in = uri.openStream()) {
+                Files.copy(in, destination, StandardCopyOption.REPLACE_EXISTING);
                 logger.info("download:  {} -> {}", url, destination);
                 result = destination.toFile().length();
             } catch (IOException e) {
                 result = -1;
-                e.printStackTrace();
                 throw new PersistentException(e);
             }
         }
